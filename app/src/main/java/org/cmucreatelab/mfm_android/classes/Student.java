2fc25320bfb61a45ad1096c46a659aab79af06da
mtasota <miketasota@gmail.com>
2016-02-04 11:14:25 -0500

    Wrote rest of mfm request handler methods.

--- app/src/main/java/org/cmucreatelab/mfm_android/classes/Student.java
+++ app/src/main/java/org/cmucreatelab/mfm_android/classes/Student.java
@@ -6,11 +6,31 @@ import java.util.ArrayList;
  * Created by mike on 1/28/16.
  */
 public class Student implements Sender {
+
     int id;
     String firstName,lastName;
     String photoUrl;
     String udpatedAt;
     ArrayList<User> users;
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+    public String getUdpatedAt() {
+        return udpatedAt;
+    }
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
+
+    // methods for Sender interface
 
     @Override
     public Type getSenderType() {
--- app/src/main/java/org/cmucreatelab/mfm_android/helpers/MfmRequestHandler.java
+++ app/src/main/java/org/cmucreatelab/mfm_android/helpers/MfmRequestHandler.java
@@ -1,8 +1,11 @@
 package org.cmucreatelab.mfm_android.helpers;
 
+import android.util.Log;
+
 import com.android.volley.Request;
 import com.android.volley.Response;
 
+import org.cmucreatelab.mfm_android.classes.Student;
 import org.cmucreatelab.mfm_android.helpers.static_classes.Constants;
 import org.json.JSONObject;
 
@@ -25,11 +28,12 @@ public class MfmRequestHandler {
         Response.Listener<JSONObject> response;
 
         requestMethod = Request.Method.GET;
-        requestUrl = Constants.MFM_API_URL + "/api/v2/students?kiosk_uid="+globalHandler.kiosk.getKioskUid();
+        requestUrl = Constants.MFM_API_URL + "/api/v2/students?kiosk_uid=" + globalHandler.kiosk.getKioskUid();
         response = new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 // TODO response handling
+                Log.i(Constants.LOG_TAG, "requestListStudents onResponse");
             }
         };
 
@@ -37,33 +41,125 @@ public class MfmRequestHandler {
     }
 
 
-    public void requestStudent() {
-        // TODO /api/v2/students/$1?kiosk_uid=$KIOSK_UID
+    public void requestStudent(Student student) {
+        int requestMethod;
+        String requestUrl;
+        Response.Listener<JSONObject> response;
+
+        requestMethod = Request.Method.GET;
+        requestUrl = Constants.MFM_API_URL + "/api/v2/students/" + student.getId() + "?kiosk_uid=" + globalHandler.kiosk.getKioskUid();
+        response = new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                // TODO response handling
+                Log.i(Constants.LOG_TAG, "requestStudent onResponse");
+            }
+        };
+
+        globalHandler.httpRequestHandler.sendJsonRequest(requestMethod, requestUrl, null, response);
     }
 
 
     public void requestListGroups() {
-        // TODO /api/v2/groups?kiosk_uid=$KIOSK_UID
+        int requestMethod;
+        String requestUrl;
+        Response.Listener<JSONObject> response;
+
+        requestMethod = Request.Method.GET;
+        requestUrl = Constants.MFM_API_URL + "/api/v2/groups?kiosk_uid="+globalHandler.kiosk.getKioskUid();
+        response = new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                // TODO response handling
+                Log.i(Constants.LOG_TAG, "requestListGroups onResponse");
+            }
+        };
+
+        globalHandler.httpRequestHandler.sendJsonRequest(requestMethod, requestUrl, null, response);
     }
 
 
     public void ping() {
-        // TODO /api/v2/ping?kiosk_uid=$KIOSK_UID&app_version=1.2.10&os_version=android_4.5
+        int requestMethod;
+        String requestUrl;
+        Response.Listener<JSONObject> response;
+
+        requestMethod = Request.Method.GET;
+        requestUrl = Constants.MFM_API_URL +
+                "/api/v2/ping?kiosk_uid=" + globalHandler.kiosk.getKioskUid() +
+                "&app_version=" + globalHandler.kiosk.getAppVersion() +
+                "&os_version=" + globalHandler.kiosk.getIosVersion();
+        response = new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                // TODO response handling
+                Log.i(Constants.LOG_TAG, "ping onResponse");
+            }
+        };
+
+        globalHandler.httpRequestHandler.sendJsonRequest(requestMethod, requestUrl, null, response);
     }
 
 
-    public void requestListSchools() {
-        // TODO /api/v2/schools?username=$MFM_USERNAME&password=$MFM_PASSWORD
+    public void requestListSchools(String username, String password) {
+        int requestMethod;
+        String requestUrl;
+        Response.Listener<JSONObject> response;
+
+        requestMethod = Request.Method.GET;
+        requestUrl = Constants.MFM_API_URL + "/api/v2/schools?username="+username+"&password="+password;
+        response = new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                // TODO response handling
+                Log.i(Constants.LOG_TAG, "requestListSchools onResponse");
+            }
+        };
+
+        globalHandler.httpRequestHandler.sendJsonRequest(requestMethod, requestUrl, null, response);
     }
 
 
-    public void login() {
-        // TODO /api/v2/login?username=$MFM_USERNAME&password=$MFM_PASSWORD&with_school=$MFM_SCHOOL_ID&app_version=1.2.10&os_version=android_4.5
+    public void login(String username, String password, String schoolId) {
+        int requestMethod;
+        String requestUrl;
+        Response.Listener<JSONObject> response;
+
+        requestMethod = Request.Method.GET;
+        requestUrl = Constants.MFM_API_URL +
+                "/api/v2/schools?username=" + username +
+                "&password=" + password +
+                "&with_school=" + schoolId +
+                "&app_version=" + globalHandler.kiosk.getAppVersion() +
+                "&os_version=" + globalHandler.kiosk.getIosVersion();
+        response = new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                // TODO response handling
+                Log.i(Constants.LOG_TAG, "login onResponse");
+            }
+        };
+
+        globalHandler.httpRequestHandler.sendJsonRequest(requestMethod, requestUrl, null, response);
     }
 
 
     public void logout() {
-        //TODO /api/v2/logout?kiosk_uid=$KIOSK_UID
+        int requestMethod;
+        String requestUrl;
+        Response.Listener<JSONObject> response;
+
+        requestMethod = Request.Method.GET;
+        requestUrl = Constants.MFM_API_URL + "/api/v2/logout?kiosk_uid="+globalHandler.kiosk.getKioskUid();
+        response = new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                // TODO response handling
+                Log.i(Constants.LOG_TAG, "logout onResponse");
+            }
+        };
+
+        globalHandler.httpRequestHandler.sendJsonRequest(requestMethod, requestUrl, null, response);
     }
 
 }

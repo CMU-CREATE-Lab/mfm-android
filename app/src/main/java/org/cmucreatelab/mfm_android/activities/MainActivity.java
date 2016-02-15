51d8eb04dfc597405db363491a4638fd9b090305
mnahta <mnahta@andrew.cmu.edu>
2016-02-15 14:09:33 -0500

    transferring everything from v1.1 to mfm-android

--- app/src/main/java/org/cmucreatelab/mfm_android/activities/MainActivity.java
+++ app/src/main/java/org/cmucreatelab/mfm_android/activities/MainActivity.java
@@ -5,34 +5,111 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import org.cmucreatelab.mfm_android.R;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        String kioskID = "f6321b67d68cd8092806094f1d1f16c5";
+        String studentsURL = "http://dev.messagefromme.org/v2/api/students?kiosk_uid=" + kioskID;
+        String groupURL = "http://dev.messagefromme.org/v2/api/groups?kiosk_uid=" + kioskID;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (isNetworkAvailable()) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(studentsURL).build();
+            Request request1 = new Request.Builder()
+                    .url(groupURL).build();
+
+            Call call = client.newCall(request);
+            Call call1 = client.newCall(request1);
+
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+
+                    try {
+
+                        if (response.isSuccessful()) {
+                            Log.v(TAG, "Students: ");
+                            Log.v(TAG, response.body().string());
+                        } else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+                    }
+
+                }
+            });
+
+            call1.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call1, IOException e) {
+                }
+
+                @Override
+                public void onResponse(Call call1, Response response) throws IOException {
+
+                    try {
+
+                        if (response.isSuccessful()) {
+                            Log.v(TAG, "Groups: ");
+                            Log.v(TAG, response.body().string());
+                        } else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+                    }
+
+                }
+            });
+        }
+        else {
+            Toast.makeText(this, "Network Is Unavailable!", Toast.LENGTH_LONG).show();
         }
 
-        return super.onOptionsItemSelected(item);
+        Log.d(TAG, "Main UI code is running!");
+
+    }
+
+    //to handle cases when there is no network available
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo  = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        }
+        return isAvailable;
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 }
